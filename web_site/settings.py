9d21853b7f661eb5841880e96d325ce352a76742
Harrmit Chauhan <46776916+harmit17@users.noreply.github.com>
2023-04-19 00:07:51 -0400

    fixed #17 - added jenkins file - bug1

--- web_site/settings.py
+++ web_site/settings.py
@@ -144,4 +144,10 @@ MESSAGE_TAGS = {
 }
 
 # email Configuration
-
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER=os.environ.get('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD=os.environ.get('EMAIL_HOST_PASSWORD')
+EMAIL_USE_TLS = True
+EMAIL_USE_SSL = False
