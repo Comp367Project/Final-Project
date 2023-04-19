ebcc3e1ebe0a12794c940e93b3ec4df30af1cbf8
mkondrag <mkondrag@my.centennialcollege.ca>
2023-04-18 23:45:16 -0400

    close #7 added application key to run application

--- web_site/settings.py
+++ web_site/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = "4g9ek!=!@1kn0@c6yja(p!*)%@af1^hcqhcde0k$wr0@mfx0c4"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
